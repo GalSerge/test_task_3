611cbfb4164088ebf35774b4267026cde364d645
Serge <galserge.sidorov@gmail.com>
2023-06-21 14:04:25 +0400

    Миграция для базы notes

---
+++ database/migrations/2023_06_21_090304_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('fio');
+            $table->string('company');
+            $table->string('email');
+            $table->date('date_birth');
+            $table->string('photo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
